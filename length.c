fc28d425c070b0bfbf7c9b43434da6c3485ba347
Lochana464 <lochanareddy1719@gmail.com>
2025-07-03 10:27:50 +0530

    area and perimeter

---
+++ length.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g;
+	
+printf("enter the value of a,b,c,d,e:");
+scanf("%d,%d,%d,%d,%d",&a,&b,&c,&d,&e);
+	
+
+g=(a+b+c+d+e)/f;
+printf("enter the value of g");
+
+return 0;
+}
